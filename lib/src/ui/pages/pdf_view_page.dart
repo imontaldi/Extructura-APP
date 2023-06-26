37c5c78f6ad44e430e78629ecef95579f9a172f6
Ignacio Montaldi <87707566+imontaldi@users.noreply.github.com>
2023-06-26 09:47:38 -0300

    Update pdf_view_page.dart

--- lib/src/ui/pages/pdf_view_page.dart
+++ lib/src/ui/pages/pdf_view_page.dart
@@ -19,6 +19,7 @@ class PdfViewPage extends StatefulWidget {
   PdfViewPageState createState() => PdfViewPageState();
 }
 
+// Ejemplo commit
 class PdfViewPageState extends StateMVC<PdfViewPage> {
   late PdfViewPageController _con;
 
