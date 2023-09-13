2bbc1e30cc8e681787d57a3887f7ac7ad376a2a0
Ignacio Montaldi <87707566+imontaldi@users.noreply.github.com>
2023-09-13 11:07:18 -0300

    .

--- lib/values/k_api.dart
+++ lib/values/k_api.dart
@@ -1,8 +1,8 @@
 const kApiUrl = "http://localhost:8000/"; // Celu
 // const kApiUrl = "http://10.0.2.2:8000/"; // Bluestacks
 
-//Para que funcione una vez levantada api
-// adb reverse tcp:8000 tcp:8000
+//Para que funcione una vez levantada api (-s idDispositivo)
+// adb -s ZY323KTFWP reverse tcp:8000 tcp:8000
 
 const kApiHeader = "${kApiUrl}header";
 const kApiItems = "${kApiUrl}items";
